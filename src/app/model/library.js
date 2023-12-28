e3031bd314ca32eea4da98f1cb5ba866c211610e
Hana <pilkova.hana@gmail.com>
2023-12-28 16:56:07 +0100

    library db model

--- src/app/model/library.js
+++ src/app/model/library.js
@@ -1,13 +1,69 @@
 module.exports = mongoose => {
   return mongoose.model(
-    "library",
+    "users",
+    mongoose.Schema(
+      {
+        name: String,
+        surname: String,
+        role: String,
+        birthNumber: String,
+        address: {
+          street: String,
+          zipcode: String,
+          city: String,
+          streetNumber: String
+        },
+        userName: String,
+        password: String,
+        activated: Boolean
+      },
+    )
+  );
+};
+
+module.exports = mongoose => {
+  return mongoose.model(
+    "books",
     mongoose.Schema(
       {
         title: String,
-        description: String,
-        published: Boolean
+        author: String,
+        numberOfPages: Number,
+        year: Number,
+        cover: Buffer,
+        availableCopies: Number,
+        totalCopies: Number
       },
-      {timestamps: true}
+    )
+  );
+};
+
+module.exports = mongoose => {
+  return mongoose.model(
+    "borrowedBooks",
+    mongoose.Schema(
+      {
+        bookId: String,
+        userId: String,
+        borrowDate: Date,
+        returnDate: Date,
+        isReturned: Boolean,
+      },
+    )
+  );
+};
+
+module.exports = mongoose => {
+  return mongoose.model(
+    "notifications",
+    mongoose.Schema(
+      {
+        userId: String,
+        description: String,
+        published: Boolean,
+        type: String
+      },
+      {timeStamp: true}
     )
   );
 };
