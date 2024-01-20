52d205b69370345c927c2125d1df484c5d0b38ac
Hana <pilkova.hana@gmail.com>
2024-01-21 00:06:44 +0100

    app header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import { SignUpComponent } from './view/sign-up/sign-up.component';
 import { HomePageComponent } from './view/home-page/home-page.component';
+import { HeaderComponent } from './view/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, SignUpComponent, HomePageComponent],
+  declarations: [AppComponent, SignUpComponent, HomePageComponent, HeaderComponent],
   imports: [BrowserModule, AdminModule, UserModule, AppRoutingModule, MatIconModule, BrowserAnimationsModule,
     HttpClientModule, MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
   providers: [],
