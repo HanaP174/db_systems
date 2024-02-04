698242ea13b968825e1e3242761a84a496911cea
Karel Šohaj <sohaj@cross.cz>
2024-02-04 10:47:52 +0100

    Add BE CRUD operation for book editor

--- src/app/admin/books-editor/books-editor.component.ts
+++ src/app/admin/books-editor/books-editor.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Book} from "../../shared/model/LibraryModel";
-import {MatTableDataSource} from "@angular/material/table";
-import {AuthService} from "../../shared/services/auth-service";
-import {BookService} from "../../shared/services/book.service";
+import { Component, OnInit } from '@angular/core';
+import { Book } from "../../shared/model/LibraryModel";
+import { MatTableDataSource } from "@angular/material/table";
+import { AuthService } from "../../shared/services/auth-service";
+import { BookService } from "../../shared/services/book.service";
 
 @Component({
   selector: 'app-books-editor',
@@ -16,7 +16,7 @@ export class BooksEditorComponent implements OnInit {
   dataSource = new MatTableDataSource<Book>;
 
   constructor(private authService: AuthService,
-              private bookService: BookService) {
+    private bookService: BookService) {
   }
 
   ngOnInit(): void {
@@ -36,7 +36,53 @@ export class BooksEditorComponent implements OnInit {
   }
 
   addBook() {
+    // TODO Get book from a form
+    const bookToAdd = new Book();
 
+    this.bookService.addBook(bookToAdd).subscribe(d => {
+      if (isNaN(d)) {
+        this.dataSource.data.push(d);
+        this.dataSource._updateChangeSubscription();
+      } else {
+        console.log(d)
+      }
+    });
+  }
+
+  editBook() {
+    // TODO Get bookId and book from a form
+    const bookId = '65bf5b0daec404630b13df15';
+    const bookToEdit = new Book();
+
+    this.bookService.updateBook(bookId, bookToEdit).subscribe(d => {
+      if (!isNaN(d)) {
+        const idx = this.dataSource.data.findIndex(b => b.id === bookId);
+        this.dataSource.data[idx].title = bookToEdit.title;
+        this.dataSource.data[idx].author = bookToEdit.author;
+        this.dataSource.data[idx].numberOfPages = bookToEdit.numberOfPages;
+        this.dataSource.data[idx].cover = bookToEdit.cover;
+        this.dataSource.data[idx].year = bookToEdit.year;
+        this.dataSource.data[idx].availableCopies = bookToEdit.availableCopies;
+        this.dataSource.data[idx].totalCopies = bookToEdit.totalCopies;
+      } else {
+        console.log(d)
+      }
+    });
+  }
+
+  deleteBook() {
+    // TODO Get bookId
+    const bookId = '65bf5b0daec404630b13df15';
+
+    this.bookService.deleteBook(bookId).subscribe(d => {
+      if (!isNaN(d)) {
+        const idx = this.dataSource.data.findIndex(b => b.id === bookId);
+        this.dataSource.data.splice(idx, 1);
+        this.dataSource._updateChangeSubscription();
+      } else {
+        console.log(d.message);
+      }
+    });
   }
 
 }
