cd015a9af856028484862d236397c330ea6ab21f
Hana <pilkova.hana@gmail.com>
2024-02-04 20:11:38 +0100

    edit book dialog component

---
+++ src/app/admin/books-editor/edit-book-dialog/edit-book-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-book-dialog',
+  templateUrl: './edit-book-dialog.component.html',
+  styleUrls: ['./edit-book-dialog.component.css']
+})
+export class EditBookDialogComponent {
+
+}
